70a1a2579c235932c255c8e29250536b6ae4197d
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-01-03 14:35:22 +0100

    Add files via upload
    Recent update on the 3rd of January 2024

---
+++ Tenece_html/ten.js
@@ -0,0 +1,136 @@
+new Swiper("#swipe-serv", {
+    speed: 1000,
+    spaceBetween: 25,
+    effect: 'slide',
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    //loop: false,
+    autoplay: true,
+    keyboard: {
+        enabled: true
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 1,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableInInteraction: false
+            },
+            //autoplayResume: true,
+        },
+        770: {
+            slidesPerView: 1,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableInInteraction: false
+            },
+            //autoplayResume: true,
+        },
+        771: {
+            //autoplayPause: false,
+        }
+    }
+});
+
+/*let slider = tns({
+    container: ".serv-man",
+    "slideBy": "3",
+    "speed" : 2000,
+    "nav": true,
+    autoplay: true,
+    controls: true,
+    loop: true,
+    autoplayButtonOutput: false,
+    autoplay:true,
+    autoplayTimeout: 1000,
+    responsive: {
+        768: {
+            items: 3,
+            gutter: 10
+        },
+
+        400: {
+            items: 1
+        }
+        
+    }
+});*/
+
+new Swiper("#swipe", {
+    speed: 500,
+    centeredSlides: true,
+    spaceBetween: 35,
+    effect: 'slide',
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    lazyLoading: true,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableInInteraction: false
+    },
+    keyboard: {
+        enabled: true
+    },
+    pagination: {
+        el: "#swipe .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '#nav-right',
+        prevEl: '#nav-left'
+    }
+});
+
+new Swiper("#swipe-me", {
+    speed: 500,
+    spaceBetween: 40,
+    centeredSlides: true,
+    effect: 'slide',
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: false,
+    autoplay: {
+        delay: 5000,
+        disableInInteraction: false
+    },
+    keyboard: {
+        enabled: true
+    },
+    pagination: {
+        el: "#swipe-me .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '#naviright',
+        prevEl: '#navileft'
+    }
+});
+
+
+const brands = document.querySelector('ul.brands'),     
+        partners_brands= document.querySelector('ul.partners_brands'),
+        news_brands = document.querySelector('ul.news_brands')
+
+        total_brand = brands.children.length,
+        total_partners = partners_brands.children.length;
+        total_news = news_brands.children.length;
+
+        document.documentElement.style.setProperty('--total-clients', total_brand);
+        document.documentElement.style.setProperty('--total-partners', total_partners);
+        document.documentElement.style.setProperty('--total-news', total_news);
+
+        for (let i = 0; i < total_brand; i++){
+            brands.appendChild(brands.children[i].cloneNode(true));
+        }
+
+        for (let i = 0; i < total_partners; i++){
+            partners_brands.appendChild(partners_brands.children[i].cloneNode(true));
+        }
+
+        for (let i = 0; i < total_news; i++){
+            news_brands.appendChild(news_brands.children[i].cloneNode(true));
+        }
